9a8c67da403a4f57c1bd9bf120c8f21a90faa0d3
Gatlat Both <148276251+Gatlat-cs2023@users.noreply.github.com>
2025-05-14 16:50:25 +0300

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,1 +1,28 @@
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="your_username",        # Replace with your MySQL username
+        password="your_password",    # Replace with your MySQL password
+        database="ALX_prodev"
+    )
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    while True:
+        rows = cursor.fetchmany(batch_size)
+        if not rows:
+            break
+        yield rows
+
+    cursor.close()
+    connection.close()
+
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):      # 1st loop
+        for user in batch:                                  # 2nd loop
+            if user['age'] > 25:                            # condition to filter
+                print(user)
 
