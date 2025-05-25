01a6a74db17cb1a369897cc59b2a5a399cf2c2cf
Gatlat-cs2023 <gatlatboth@gmail.com>
2025-05-25 12:36:23 +0300

    Committed

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -4,17 +4,19 @@ import mysql.connector
 def stream_users():
     connection = mysql.connector.connect(
         host="localhost",
-        user="your_username",
-        password="your_password",
-        database="ALX_prodev"
+        user="root",
+        password="Faithoverfear@1998",
+        database="alx_airbnb_database"
     )
 
     cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM user_data")
+    cursor.execute("SELECT * FROM user")
 
     for row in cursor:
         yield row
 
     cursor.close()
     connection.close()
-
+# Test
+for user in stream_users():
+    print(user)
