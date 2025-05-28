891c52c2c0a3c579546cb91f58596c7235b4ea9b
Gatlat-cs2023 <gatlatboth@gmail.com>
2025-05-28 15:57:55 +0300

    COMMITTED

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -10,3 +10,17 @@ def access_nested_map(nested_map, path):
 def get_json(url):
     response = requests.get(url)
     return response.json()
+
+# utils.py
+
+def memoize(fn):
+    """Memoization decorator for a method."""
+    attr_name = "_memoized_" + fn.__name__
+
+    @property
+    def memoized(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    return memoized
