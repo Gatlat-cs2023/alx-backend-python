7f1b046560d435a993488d797786e311a4061995
Gatlat-cs2023 <gatlatboth@gmail.com>
2025-05-31 11:07:24 +0300

    COMITTED

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
-
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['users__username']  # Adjust as needed
+    filterset_fields = ['users']
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -18,6 +20,9 @@ class ConversationViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['message_body']
+    filterset_fields = ['conversation', 'sender']
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -137,6 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # DRF Configuration (Optional)
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
