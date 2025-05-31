c743d65fb3b99e32a971baaa6698178f81541bfc
Gatlat-cs2023 <gatlatboth@gmail.com>
2025-05-31 11:12:58 +0300

    COMITTED

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from chats.views import ConversationViewSet, MessageViewSet
+from .views import ConversationViewSet, MessageViewSet
 
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
