28aa9cd943dba46fb8c9018ccb79956ee81aac8a
Gatlat Both <gatlatboth@gmail.com>
2025-06-13 23:32:42 +0300

    new commit

--- Django-signals_orm-0x04/chat/apps.py
+++ Django-signals_orm-0x04/chat/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
 
-class MessagingConfig(AppConfig):
+class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'messaging'
+    name = 'chat'
 
     def ready(self):
-        import messaging.signals
+        import chat.signals
--- Django-signals_orm-0x04/messaging/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('history/<int:message_id>/', views.message_history_view, name='message_history'),
-]
