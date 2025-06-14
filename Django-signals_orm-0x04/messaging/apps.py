9d4223f29ee85ad9d2b433ec2b596f5f92a20b7e
Gatlat Both <gatlatboth@gmail.com>
2025-06-14 19:25:27 +0300

    New Commit

--- Django-signals_orm-0x04/messaging/apps.py
+++ Django-signals_orm-0x04/messaging/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chat'
+    name = 'messaging'
 
     def ready(self):
-        import chat.signals
+        import message.signals
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.contrib import messages
 
-# Create your views here.
+@login_required
+def delete_user(request):
+    user = request.user
+    user.delete()
+    messages.success(request, "Your account has been deleted.")
+    return redirect('home')  # Replace 'home' with your homepage route name
